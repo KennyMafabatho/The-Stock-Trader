88b7ed2e8a224dacc93f6ed22f187d1881e1589d
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2021-05-18 14:08:06 +0200

    Added content to components

--- The-Stock-Trader/src/App.vue
+++ The-Stock-Trader/src/App.vue
@@ -1,13 +1,21 @@
 <template>
     <div class="container">
-
+      <app-header></app-header>
+      <div class="row">
+        <div class="col-xs-12">
+          <router-view></router-view>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
+import Header from './components/TheHeader.vue';
 
     export default {
-
+      components:{
+        appHeader: Header
+      }
     }
 </script>
 
---
+++ The-Stock-Trader/src/components/TheHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <router-link to="/" class="navbar-brand"> Stock Trader</router-link>
+      </div>
+
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <router-link to="/portfolio" active-class="active" tag="li">Portfolio</router-link>
+          <router-link to="/stocks" active-class="active" tag="li">Stocks</router-link>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">End Day</a></li>
+          <li class="drop-down">
+            <a href="#"
+              class="dropdown-toggle"
+              data-toggle="dropdown"
+              role="button"
+              aria-haspopup="true"
+              aria-expanded="false">
+              Save & Load <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li> <a href="#">Save Data</a></li>
+              <li> <a href="#">Load Data</a></li>
+              
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
