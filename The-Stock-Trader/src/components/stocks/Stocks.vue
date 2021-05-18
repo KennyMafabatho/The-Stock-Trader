b892dd7fb9c8f87f28ed5e8a9baa1bba1d179627
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2021-05-18 16:20:48 +0200

    Added data in stock and also created style for stock

--- The-Stock-Trader/src/components/stocks/Stocks.vue
+++ The-Stock-Trader/src/components/stocks/Stocks.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>Stocks</h1>
+  <div>
+    <app-stock v-for="stock in stocks"></app-stock>
+  
+  </div>
 </template>
+
+<script>
+import Stock from './Stock.vue';
+
+export default {
+  components:{
+    appStock: Stock
+  },
+  data(){
+    return{
+      stocks:[
+        {id: 1, name: 'BMW', price: 110},
+        {id: 2, name: 'Apple', price: 200},
+        {id: 3, name: 'Google', price: 250},
+        {id: 4, name: 'Twitter', price: 80},
+      ]
+      
+    }
+  }
+}
+</script>
