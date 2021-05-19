9f2978f074203bb291055df70de617ccdf98442e
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2021-05-19 14:17:32 +0200

    Installed vuex

--- The-Stock-Trader/src/components/stocks/Stock.vue
+++ The-Stock-Trader/src/components/stocks/Stock.vue
@@ -3,19 +3,43 @@
     <div class="panel panel-success">
         <div class="panel panel-heading">
           <h3 class="panel-title"> 
-              Name 
-            <small>(Price: PRICE)</small>
+              {{ stock.name }} 
+            <small>(Price: {{ stock.price }})</small>
           </h3>
         </div>
         <div class="panel-body">
             <div class="pull-left">
               <input type="number" class="form-control"
-              placeholder="Quantity">
+               placeholder="Quantity" v-model="quantity"/>
             </div>
             <div class="pull-right">
-              <button class="btn btn-success">Buy</button>
+              <button class="btn btn-success" @click="buyStock" 
+                :disabled="quantity <= 0 || !Number.isInteger(quantity)">
+                Buy
+               </button>
             </div>
          </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: ['stock'],
+  data(){
+    return{
+      quantity: 0
+    }
+  },
+  methods:{
+    buyStock (){
+      const order = {
+        stockId : this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+
+    }
+  }
+}
+</script>
